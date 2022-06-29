9edde314fa997332399a5d19eaa9c46c51eb9ee1
StevanKz <87872818+StevanKz@users.noreply.github.com>
2022-06-29 23:13:54 +0700

    Fix format

--- Mention/__main__.py
+++ Mention/__main__.py
@@ -14,13 +14,6 @@ from uvloop import install
 from Mention import app
 
 
-MSG = """
-**{} Successfully activated !
-
-Join:** https://t.me/StvnsYu
-"""
-
-
 async def start_bot():
     try:
         await app.start()
@@ -30,9 +23,9 @@ async def start_bot():
 
 
 if __name__ == "__main__":
-    app.logs.info("Starting Mention-Bot")
+    app.logs.info("Starting Hugo Mention")
     install()
     app.git()
     app.heroku()
-    app.logs.info(MSG.format(app.name))
+    app.logs.info("Hugo Mention Started Successfully !")
     app.loop.run_until_complete(start_bot())
