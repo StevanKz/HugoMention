8762e769ef2cca7c500e5928963631d8b0dd844a
StevanKz <87872818+StevanKz@users.noreply.github.com>
2022-06-29 23:17:51 +0700

    Reformat

--- Mention/__init__.py
+++ Mention/__init__.py
@@ -13,28 +13,14 @@ import sys
 from pyromod import listen
 from pyrogram import Client
 
-from .utils import Utils
+from config import API_ID, API_HASH, BOT_TOKEN, WORKERS
 
 
-class HugoMention(Client, Utils):
-    def __init__(self):
-        super().__init__(
-            "Hugo-Mention",
-            api_id=self.API_ID,
-            api_hash=self.API_HASH,
-            bot_token=self.BOT_TOKEN,
-            workers=self.WORKERS,
-            plugins={"root": "Mention.plugins"},
-        )
-
-    async def start(self):
-        await super().start()
-        bot_me = await self.get_me()
-        self.name = bot_me.first_name
-
-    async def stop(self, *args):
-        await super().stop()
-        self.logs.info("Bot stopped.")
-
-
-app = HugoMention()
+app = Client(
+    "Hugo-Mention",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=WORKERS,
+    plugins={"root": "Mention.plugins"},
+)
